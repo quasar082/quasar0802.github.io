227cfa7b10c8c6af7d75959541f8396c5683207f
Hà Minh Quân (Quasar) <99237494+quasar082@users.noreply.github.com>
2026-05-12 04:47:47 +0700

    hiddien contract footer

--- src/components/sections/home/site-footer.tsx
+++ src/components/sections/home/site-footer.tsx
@@ -4,9 +4,9 @@ export function SiteFooter() {
       <p className="m-0 w-full whitespace-nowrap text-[clamp(2rem,22vw,24rem)] md:text-[clamp(8rem,21vw,25rem)] lg:text-[clamp(8rem,24vw,25rem)] font-medium leading-none tracking-[-0.03em] text-black">
         QUASAR
       </p>
-      <div className="mt-3 border-t border-black/35 pt-3">
+      {/* <div className="mt-3 border-t border-black/35 pt-3">
         <p className="m-0 text-sm tracking-wide text-black/65">© 2026 Quasar. All rights reserved.</p>
-      </div>
+      </div> */}
     </footer>
   );
 }
